a52c2f0911f21c3991704ea20dcbef269b414d5e
Zachary Pennington <60146569+zachary-AP@users.noreply.github.com>
2021-02-13 21:58:56 -0500

    Add files via upload

---
+++ sortscript.js
@@ -0,0 +1,19 @@
+// initialize the counter and the array
+var numbernames=0;
+var names = new Array();
+
+function SortNames() {
+   // Get the name from the text field
+   thename=document.theform.newname.value;
+   
+   // Add the name to the array
+   //names[numbernames]=thename;
+   
+   //Capitalize name and add to array
+   names[numbernames]=thename.toUpperCase();
+   // Increment the counter
+   numbernames++;
+   // Sort the array
+   names.sort();
+   document.theform.sorted.value=names.join("\n");
+}
